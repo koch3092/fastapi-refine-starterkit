ed2b5c22927198206cebfa13f920e6dfef014e96
koch3092 <developer@dorakoch.com>
2026-01-18 20:16:20 +0800

    feat: [后端] 更新 CORS 设置以支持自定义响应头

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,6 +28,7 @@ if settings.all_cors_origins:
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
+        expose_headers=["X-Total-Count"],
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
